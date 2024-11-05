2ab980d3fe9ac2fafea80619c3037b117cf99d2f
adaedjang <aedjamay@myuax.com>
2024-11-05 11:16:24 +0100

    Elercicio 2 (luego modifico el main.cpp)

--- Ejercicio 2/dia.cpp
+++ Ejercicio 2/dia.cpp
@@ -1,5 +1,34 @@
 //
 // Created by 34601 on 05/11/2024.
 //
-
+#include <iostream>
 #include "dia.h"
+
+void imprimirDiaSemana(int numero) {
+    switch (numero) {
+    case 1:
+        std::cout << "Lunes" << std::endl;
+        break;
+    case 2:
+        std::cout << "Martes" << std::endl;
+        break;
+    case 3:
+        std::cout << "Miércoles" << std::endl;
+        break;
+    case 4:
+        std::cout << "Jueves" << std::endl;
+        break;
+    case 5:
+        std::cout << "Viernes" << std::endl;
+        break;
+    case 6:
+        std::cout << "Sábado" << std::endl;
+        break;
+    case 7:
+        std::cout << "Domingo" << std::endl;
+        break;
+    default:
+        std::cout << "Número inválido. Por favor, ingresa un número del 1 al 7." << std::endl;
+    }
+}
+
--- Ejercicio 2/dia.h
+++ Ejercicio 2/dia.h
@@ -1,16 +1,11 @@
 //
 // Created by 34601 on 05/11/2024.
 //
-
 #ifndef DIA_H
 #define DIA_H
 
+// Declaración de la función
+void imprimirDiaSemana(int numero);
 
+#endif
 
-class dia {
-
-};
-
-
-
-#endif //DIA_H
